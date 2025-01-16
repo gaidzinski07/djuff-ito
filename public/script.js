1c0831e1f9d97fcee686aaec181d0afafebbae33
Rafael Rocha <efp.rafael.rd@gmail.com>
2025-01-15 23:22:17 -0300

    add sistema de conversa entre usuários próximos

--- public/script.js
+++ public/script.js
@@ -3,13 +3,15 @@ const socket = io();
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 let users;
+let nearUsers = [];
+let talkingTo = undefined;
 
-//função para desenhar todos os círculos
+// Função para desenhar todos os círculos
 function drawBalls() {
     console.log(users); //debug
 
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa o canvas uma vez antes de desenhar todas os círculos
-    //desenha todos os círculos
+    // Desenha todos os círculos
     users.forEach((value) => {
         ctx.beginPath();
         ctx.arc(value.x + 5, value.y + 5, 25, 0, Math.PI * 2);
@@ -23,6 +25,53 @@ function drawBalls() {
         ctx.fill();
         ctx.closePath();
     });
+
+}
+
+// Função para procurar os usuários próximos
+function searchProximity(){
+    foundTalk = false;
+    
+    nearUsers = [];
+    // Itera por todos os usuários e verifica a distância
+    users.forEach((value) => {
+        if(value.id != socket.id){
+            xdiff = value.x - users.get(socket.id).x
+            ydiff = value.y - users.get(socket.id).y
+            if( (xdiff < 50 && xdiff > -50) && (ydiff < 50 && ydiff > -50)) nearUsers.push(value); // add se perto
+        }
+    });
+
+    const buttonContainer = document.getElementById('nearUsers');
+    buttonContainer.style.display = 'flex';
+    buttonContainer.style.justifyContent = 'center';
+    buttonContainer.innerHTML = '';
+    
+    // Cria um botão para iniciar conversa com todos os usuários próximos
+    nearUsers.forEach((user) => {
+        if(user.id == talkingTo) foundTalk = true;
+        const button = document.createElement('button');
+        button.style.backgroundColor = user.color;
+        button.innerText = "Conversar";
+        button.addEventListener('click', () => {
+            if(talkingTo == undefined) socket.emit('start-conversation', { starter: socket.id, receiver:  user.id }); // iniciar conversa
+        });
+
+
+        buttonContainer.appendChild(button);
+    });
+
+    // Termina a conversa se o usuário sair de proximidade
+    if(talkingTo != undefined && !foundTalk){
+        console.log(talkingTo);
+        socket.emit('end-conversation', { starter: socket.id, receiver:  talkingTo });
+    }
+}
+
+// Função para desenhar os círculos e procurar usuários próximos
+function updateState(){
+    drawBalls();
+    searchProximity();
 }
 
 //EVENTOS DE CLICK JOYSTICK
@@ -31,7 +80,7 @@ document.getElementById('left').addEventListener('click', () => {
     users.get(socket.id).x = users.get(socket.id).x < 0 ? 0 : users.get(socket.id).x;
 
     socket.emit('joystick-input', { id: socket.id, x: users.get(socket.id).x, y: users.get(socket.id).y });
-    drawBalls();
+    updateState();
 });
 
 document.getElementById('up').addEventListener('click', () => {
@@ -39,7 +88,7 @@ document.getElementById('up').addEventListener('click', () => {
     users.get(socket.id).y = users.get(socket.id).y < 0 ? 0 : users.get(socket.id).y;
 
     socket.emit('joystick-input', { id: socket.id, x: users.get(socket.id).x, y: users.get(socket.id).y });
-    drawBalls();
+    updateState();
 });
 
 document.getElementById('down').addEventListener('click', () => {
@@ -47,7 +96,7 @@ document.getElementById('down').addEventListener('click', () => {
     users.get(socket.id).y = users.get(socket.id).y > 400 ? 400 : users.get(socket.id).y;
 
     socket.emit('joystick-input', { id: socket.id, x: users.get(socket.id).x, y: users.get(socket.id).y });
-    drawBalls();
+    updateState();
 });
 
 document.getElementById('right').addEventListener('click', () => {
@@ -55,7 +104,7 @@ document.getElementById('right').addEventListener('click', () => {
     users.get(socket.id).x = users.get(socket.id).x > 400 ? 400 : users.get(socket.id).x;
 
     socket.emit('joystick-input', { id: socket.id, x: users.get(socket.id).x, y: users.get(socket.id).y });
-    drawBalls();
+    updateState();
 });
 
 //EVENTOS DE CLICK DO MOUSE/TOUCH
@@ -68,11 +117,79 @@ canvas.addEventListener('click', function(event) {
     users.get(socket.id).y = y;
 
     socket.emit('joystick-input', { id: socket.id, x: users.get(socket.id).x, y: users.get(socket.id).y });
-    drawBalls();
+    updateState();
 });
 
-
+// Evento para atualizar as posições dos círculos no cliente
 socket.on('update-position', (data) => {
     users = new Map(data);
-    drawBalls();
+    updateState();
 });
+
+//CONVERSA
+
+// Evento para iniciar conversa caso o cliente esteja envolvido
+socket.on('conversation-started', (data) => {
+    if(data.receiver == socket.id || data.starter == socket.id){
+        if(data.starter == socket.id) talkingTo = data.receiver;
+        if(data.receiver == socket.id) talkingTo = data.starter;
+        
+        // Div para o chat
+        const chatDiv = document.getElementById('chat');
+        chatDiv.style.display = 'block';
+        
+        // Div para as mensagens
+        const messagesDiv = document.createElement('div');
+        messagesDiv.id = 'messages';
+        messagesDiv.style.height = '200px';
+        messagesDiv.style.overflowY = 'scroll';
+        messagesDiv.style.width = '30%';
+        messagesDiv.style.backgroundColor = 'white';
+        messagesDiv.style.margin = '0 auto';
+        chatDiv.appendChild(messagesDiv);
+
+        const inputs = document.createElement('div');
+        inputs.style.display = 'flex';
+        inputs.style.justifyContent = 'center';
+        chatDiv.appendChild(inputs);
+
+        // Elemento para input
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = 'messageInput';
+        inputs.appendChild(input);
+        
+
+        // Botão para enviar
+        const sendButton = document.createElement('button');
+        sendButton.innerText = 'Enviar';
+        sendButton.addEventListener('click', () => {
+            const message = input.value;
+            socket.emit('send-message', { sender: socket.id, receiver: talkingTo, message: message });
+            input.value = '';
+        });
+        inputs.appendChild(sendButton);
+    }
+  });
+
+  // Evento para notificar o término da conversa atual
+  socket.on('conversation-ended', (data) => {
+        if(data.receiver == socket.id || data.starter == socket.id){
+        talkingTo = undefined;
+
+        const chatDiv = document.getElementById('chat');
+        chatDiv.innerHTML = '';
+        chatDiv.style.display = 'none';
+    }
+  })
+
+  // Evento para notificar uma mensagem enviada do cliente para um usuário
+  socket.on('message-sent', (data) => {
+    if(data.to == socket.id || data.from == socket.id){
+        const messagesDiv = document.getElementById('messages');
+        const messageElement = document.createElement('div');
+        messageElement.innerText = `${data.message}`;
+        messageElement.style.color = users.get(data.from).color;
+        messagesDiv.appendChild(messageElement);
+    }
+  })
--- server.js
+++ server.js
@@ -10,9 +10,10 @@ app.use(express.static('public')); // Servir arquivos estáticos (frontend)
 
 // Variável para armazenar os círculos de todos os usuários
 const users = new Map();
+let conversations = new Map();
 
 io.on('connection', (socket) => {
-  console.log('Um usuário conectou:', socket.id);
+  console.log('Um usuário conectou:', socket.handshake.address);
 
   // Adiciona um objeto para o usuário recém conectado com um círculo em (200,200) com uma cor aleatória
   users.set(socket.id, {id: socket.id, x: 200, y: 200, color:`rgb(${Math.random() * 200/2 + 50} , ${Math.random() * 255 + 25}, ${Math.random() * 155 + 10})`});
@@ -20,6 +21,25 @@ io.on('connection', (socket) => {
   // Enviar a posição atual dos círculos para o novo usuário
   socket.emit('update-position', Array.from(users.entries()));
   
+  // Evento para começar conversa entre dois clientes
+  socket.on('start-conversation', (data) => {
+    if(conversations.get(data.receiver) == undefined){
+      conversations.set(data.starter, data.receiver);
+      conversations.set(data.receiver, data.starter);
+      // Notifica todos os usuários do início da conversa
+      io.emit ('conversation-started', { starter: data.starter, receiver: data.receiver });
+    }
+  });
+  
+  // Evento para terminar uma conversa entre dois clientes
+  socket.on('end-conversation', (data) => {
+    console.log(data.starter, "parou de conversar com", data.receiver);
+    conversations.delete(data.starter);
+    conversations.delete(data.receiver);
+    // Notifica todos os usuários do término da conversa
+    io.emit ('conversation-ended', { starter: data.starter, receiver: data.receiver });
+  })
+
   socket.on('joystick-input', (data) => {
     // Atualizar a posição do círculo de um usuário
     users.get(data.id).x = data.x;
@@ -27,13 +47,28 @@ io.on('connection', (socket) => {
     io.emit('update-position', Array.from(users.entries())); // Reencaminhar para todos os dispositivos
   });
 
+  // Evento para enviar uma mensagem
+  socket.on('send-message', (data) => {
+    // Envia a mensagem para todos os usuários
+    io.emit('message-sent', { from: data.sender, to: data.receiver, message: data.message });
+  });
+
+
   socket.on('disconnect', () => {
     console.log('Um usuário desconectou:', socket.id);
+    talkingTo = conversations.get(socket.id);
+    if(talkingTo != undefined){
+      conversations.delete(socket.id);
+      conversations.delete(talkingTo);
+      io.emit ('conversation-ended', { starter: socket.id, receiver: talkingTo });
+    }
     // Deletar o usuário do map
     users.delete(socket.id);
     // Atualizar todos os usuários
     io.emit('update-position', Array.from(users.entries()));
   });
+
+
 });
 
 server.listen(3000, () => {
